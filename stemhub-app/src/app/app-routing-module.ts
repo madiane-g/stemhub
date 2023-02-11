168c8b9fa7007cdaa4829566ce4c78892280e004
Madelyn Andrews <maddya30@email.unc.edu>
2023-02-11 19:06:57 +0000

    home and study

---
+++ stemhub-app/src/app/app-routing-module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StudyComponent } from './study/study.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {path: 'study', component: StudyComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
