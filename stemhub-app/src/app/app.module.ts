852f64162c0dacc7650d127883abf7e4ea2f5185
Madelyn Andrews <maddya30@email.unc.edu>
2023-02-11 13:54:35 -0500

    Added home and study buttons/navigation

--- stemhub-app/src/app/app.module.ts
+++ stemhub-app/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing-module/app-routing-module.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StudyComponent } from './study/study.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StudyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'study', component: StudyComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
